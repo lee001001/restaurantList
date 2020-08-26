c8e2f5248cd7a6dd572e53fbd06fce004cc76b38
King LEE <a0988698776@gmail.com>
2020-08-26 10:35:53 +0800

    fix image and add icon and delete stylecss2

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results });
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  console.log('req.params.restaurant_id', req.params.restaurant_id)
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+app.get('/restaurants/:id', (req, res) => {
+  console.log('req.params.restaurant_id', req.params.id)
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
 
   res.render('show', { restaurant: restaurant })
 })
